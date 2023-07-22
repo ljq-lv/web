7b7e4d5a40c2016f86315ca1f9cc878c8f9f4a68
lvjunqi <939468378@qq.com>
2023-07-22 18:06:23 +0800

    qq

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'web',
+  title: 'fitness robot',
   tagline: 'provide u',
   favicon: 'img/favicon.ico',
 
